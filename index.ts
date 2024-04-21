db37ec7cd5157e5545029c0fbf05f5bc6efe5983
Nehal <nehaltariq78@gmail.com>
2024-04-20 23:06:02 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+const { wordCounter } = await inquirer.prompt([
+  {
+    name: "wordCounter",
+    type: "input",
+    message: "Enter your sentence to count the word",
+  },
+]);
+
+const word = wordCounter.trim().split(" ");
+console.log(word);
+
+console.log(chalk.yellowBright(`your sentence word count is ${word.length}`));
